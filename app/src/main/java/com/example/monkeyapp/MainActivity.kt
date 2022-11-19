133fd6da8018a8a41523ccd2504e3115f33c9b43
DIbarraML <danton_0129hotmail.com>
2022-11-19 02:18:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/monkeyapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.monkeyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.monkeyapp.ui.theme.MonkeyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MonkeyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen() {
+    Text(text = "Expert Samurais\n" +
+            "on Develop Secure apps\n" +
+            "with Sensitive data. ")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MonkeyAppTheme {
+    }
+}
